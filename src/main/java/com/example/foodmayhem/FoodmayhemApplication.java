3c6640cbcfdeff46375aa59a4c72e7282bc6cb00
LAPTOP-743C5V7K\nerea <nerea.dzjrc@gmail.com>
2022-12-20 00:44:19 +0100

    First git

---
+++ src/main/java/com/example/foodmayhem/FoodmayhemApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodmayhem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmayhemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmayhemApplication.class, args);
+	}
+
+}
